7788645c464a0067afaecc789253951d2560c86c
rktst110 <84590431+rktst110@users.noreply.github.com>
2023-05-16 16:01:40 +0530

    f

--- navbar.js
+++
@@ -1,9 +0,0 @@
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    document.getElementById("navbar").innerHTML = this.responseText;
-  }
-};
-//xhttp.open("GET", "navbar.html", true);
-xhttp.open("GET", "MenuBar.html", true);
-xhttp.send();
