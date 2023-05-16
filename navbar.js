2393435ca97f483265153c472a55c5f147018148
rktst110 <84590431+rktst110@users.noreply.github.com>
2023-05-16 07:35:00 +0530

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,8 @@
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    document.getElementById("navbar").innerHTML = this.responseText;
+  }
+};
+xhttp.open("GET", "navbar.html", true);
+xhttp.send();
